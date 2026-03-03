28329205678fa4e21aa6af0fcdebdc9ecfd858fa
Npdminh <dangminh1203@gmail.com>
2026-03-03 21:05:44 +0700

    Add files via upload

---
+++ Karnaugh/Karnaugh/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Karnaugh
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cboVariables = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboMethod = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMinterm = new System.Windows.Forms.TextBox();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvTruthTable = new System.Windows.Forms.DataGridView();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTruthTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(909, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "PHƯƠNG PHÁP RÚT GỌN HÀM LOGIC";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cboVariables
+            // 
+            this.cboVariables.FormattingEnabled = true;
+            this.cboVariables.Items.AddRange(new object[] {
+            "2 biến",
+            "3 biến",
+            "4 biến"});
+            this.cboVariables.Location = new System.Drawing.Point(94, 56);
+            this.cboVariables.Name = "cboVariables";
+            this.cboVariables.Size = new System.Drawing.Size(121, 24);
+            this.cboVariables.TabIndex = 1;
+            this.cboVariables.SelectedIndexChanged += new System.EventHandler(this.cboVariables_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Số biến:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Phương pháp:";
+            // 
+            // cboMethod
+            // 
+            this.cboMethod.FormattingEnabled = true;
+            this.cboMethod.Items.AddRange(new object[] {
+            "Karnaugh",
+            "Quine McCluskey"});
+            this.cboMethod.Location = new System.Drawing.Point(400, 56);
+            this.cboMethod.Name = "cboMethod";
+            this.cboMethod.Size = new System.Drawing.Size(121, 24);
+            this.cboMethod.TabIndex = 4;
+            this.cboMethod.SelectedIndexChanged += new System.EventHandler(this.cboMethod_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nhập minterm:";
+            // 
+            // txtMinterm
+            // 
+            this.txtMinterm.Location = new System.Drawing.Point(161, 108);
+            this.txtMinterm.Name = "txtMinterm";
+            this.txtMinterm.Size = new System.Drawing.Size(100, 22);
+            this.txtMinterm.TabIndex = 6;
+            this.txtMinterm.TextChanged += new System.EventHandler(this.txtMinterm_TextChanged);
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(300, 132);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(104, 44);
+            this.btnSolve.TabIndex = 7;
+            this.btnSolve.Text = "Rút gọn";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtResult, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dgvTruthTable, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 182);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(909, 348);
+            this.tableLayoutPanel1.TabIndex = 16;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint_1);
+            // 
+            // dgvTruthTable
+            // 
+            this.dgvTruthTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTruthTable.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvTruthTable.Location = new System.Drawing.Point(3, 19);
+            this.dgvTruthTable.Name = "dgvTruthTable";
+            this.dgvTruthTable.RowHeadersWidth = 51;
+            this.dgvTruthTable.RowTemplate.Height = 24;
+            this.dgvTruthTable.Size = new System.Drawing.Size(539, 326);
+            this.dgvTruthTable.TabIndex = 14;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtResult.Location = new System.Drawing.Point(548, 19);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(358, 326);
+            this.txtResult.TabIndex = 16;
+            this.txtResult.TextChanged += new System.EventHandler(this.txtResult_TextChanged_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 16);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Bảng chân trị";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(548, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 16);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Kết quả rút gọn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 530);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.txtMinterm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cboMethod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboVariables);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTruthTable)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cboVariables;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboMethod;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMinterm;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.DataGridView dgvTruthTable;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Karnaugh/Karnaugh/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Karnaugh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboVariables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboMethod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtMinterm_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = int.Parse(cboVariables.Text.Split(' ')[0]);
+
+                var minterms = txtMinterm.Text
+                    .Split(',')
+                    .Select(x => int.Parse(x.Trim()))
+                    .ToList();
+
+                GenerateTruthTable(n, minterms);
+
+                if (cboMethod.Text == "Karnaugh")
+                    txtResult.Text = KarnaughSolve(minterms, n);
+                else
+                    txtResult.Text = QuineMcCluskey(minterms, n);
+            }
+            catch
+            {
+                MessageBox.Show("Nhập sai định dạng!");
+            }
+        }
+
+        private void dgvTruthTable_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void txtResult_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void GenerateTruthTable(int n, List<int> minterms)
+        {
+            dgvTruthTable.Columns.Clear();
+            dgvTruthTable.Rows.Clear();
+
+            for (int i = 0; i < n; i++)
+            {
+                dgvTruthTable.Columns.Add("Var" + i, ((char)('A' + i)).ToString());
+            }
+
+            dgvTruthTable.Columns.Add("F", "F");
+
+            int rows = (int)Math.Pow(2, n);
+
+            for (int i = 0; i < rows; i++)
+            {
+                string binary = Convert.ToString(i, 2).PadLeft(n, '0');
+
+                List<object> row = new List<object>();
+
+                foreach (char bit in binary)
+                    row.Add(bit.ToString());
+
+                row.Add(minterms.Contains(i) ? "1" : "0");
+
+                dgvTruthTable.Rows.Add(row.ToArray());
+                dgvTruthTable.Width = 100 + n * 80;
+            }
+        }
+        private string MintermToExpression(int m, int n)
+        {
+            string binary = Convert.ToString(m, 2).PadLeft(n, '0');
+            string result = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                char varName = (char)('A' + i);
+
+                if (binary[i] == '1')
+                    result += varName;
+                else
+                    result += varName + "'";
+            }
+
+            return result;
+        }
+        private string KarnaughSolve(List<int> minterms, int n)
+        {
+            if (minterms.Count == 0)
+                return "F = 0";
+
+            if (minterms.Count == Math.Pow(2, n))
+                return "F = 1";
+
+            List<string> terms = new List<string>();
+
+            foreach (var m in minterms)
+                terms.Add(MintermToExpression(m, n));
+
+            return "F = " + string.Join(" + ", terms);
+        }
+        private string CompareBinary(string a, string b)
+        {
+            int diff = 0;
+            string result = "";
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == b[i])
+                    result += a[i];
+                else
+                {
+                    diff++;
+                    result += "-";
+                }
+            }
+
+            return diff == 1 ? result : null;
+        }
+        private string QuineMcCluskey(List<int> minterms, int n)
+        {
+            var binaries = minterms
+                .Select(m => Convert.ToString(m, 2).PadLeft(n, '0'))
+                .ToList();
+
+            List<string> primeImplicants = new List<string>();
+            HashSet<string> used = new HashSet<string>();
+
+            
+            for (int i = 0; i < binaries.Count; i++)
+            {
+                for (int j = i + 1; j < binaries.Count; j++)
+                {
+                    var combined = CompareBinary(binaries[i], binaries[j]);
+                    if (combined != null)
+                    {
+                        primeImplicants.Add(combined);
+                        used.Add(binaries[i]);
+                        used.Add(binaries[j]);
+                    }
+                }
+            }
+
+            
+            foreach (var b in binaries)
+            {
+                if (!used.Contains(b))
+                    primeImplicants.Add(b);
+            }
+
+            primeImplicants = primeImplicants.Distinct().ToList();
+
+            
+            List<string> resultTerms = new List<string>();
+
+            foreach (var pattern in primeImplicants)
+            {
+                string term = "";
+
+                for (int i = 0; i < n; i++)
+                {
+                    char varName = (char)('A' + i);
+
+                    if (pattern[i] == '1')
+                        term += varName;
+                    else if (pattern[i] == '0')
+                        term += varName + "'";
+                    
+                }
+
+                if (term != "")
+                    resultTerms.Add(term);
+            }
+
+            return "F = " + string.Join(" + ", resultTerms);
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dgvTruthTable_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtResult_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
